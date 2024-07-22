1af52aced36ce98d87a4d86175e9b65c7d07c153
Christopher Druke <cdrukepike@gmail.com>
2024-07-22 06:13:40 -0400

    Import component styles & modify button icon size

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import PropTypes from 'prop-types';
 import { ThemeContext } from "../../Contexts";
 import Icon from "../Icon/Icon";
+import './Navbar.css'
 
 const capitalizeStr = str => str.charAt(0).toUpperCase() + str.slice(1);
 
@@ -11,7 +12,7 @@ function Navbar({ toggleTheme }) {
   return (
     <nav className={`navbar theme--${theme}`}>
       <div className="logo">
-        <Icon name={"logo"} descriptor="CanCodes Retro Logo" size={64} />
+        <Icon name={"logo"} descriptor="CanCodes Retro Logo" size={48} />
         <h1 className="logo--text">
           CanCodes
           <span>Retro</span>
@@ -20,15 +21,15 @@ function Navbar({ toggleTheme }) {
       <h1 className="active-board--title">Retro Board</h1>
       <div className="navbar--controls">
         <button className="navbar--btn">
-          <Icon name={"timer"} size={32} />
+          <Icon name={"timer"} size={24} />
         </button>
         <button className="navbar--btn">
-          <Icon name={"teamMembers"} size={32} />
+          <Icon name={"teamMembers"} size={24} />
         </button>
         <button
           className={`navbar--btn btn-${theme}`}
           onClick={toggleTheme}>
-          <Icon name={`theme${capitalizeStr(theme)}`} size={32} />
+          <Icon name={`theme${capitalizeStr(theme)}`} size={24} />
         </button>
       </div>
     </nav>
