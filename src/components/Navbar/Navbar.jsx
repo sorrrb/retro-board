e7f260893a048612a523e44b245fb4aea58ada94
Christopher Druke <cdrukepike@gmail.com>
2024-07-22 18:56:52 -0400

    Refactor component/implement utility styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,19 +11,35 @@ function Navbar({ toggleTheme }) {
   const theme = useContext(ThemeContext);
 
   return (
-    <nav className={`navbar theme--${theme}`}>
-      <div className="logo">
-        <Icon name={"logo"} descriptor="CanCodes Retro Logo" size={48} />
-        <h1 className="logo--text">
+    <nav className={`navbar theme--${theme} flex align-center`}>
+      <div className="logo--container flex align-center">
+        <Icon name={"logo"} size={48} />
+        <h1 className="logo--text font-xl">
           <span>CanCodes</span>
           <span>Retro</span>
         </h1>
       </div>
-      <h1 className="active-board--title">Retro Board</h1>
-      <div className="navbar--controls">
-        <IconButton btnClass={"navbar--btn"} subtext={"Timer"} iconName={"timer"} iconSize={32} />
-        <IconButton btnClass={"navbar--btn"} subtext={"Team"} iconName={"teamMembers"} iconSize={32} />
-        <IconButton btnClass={"navbar--btn"} subtext={"Theme"} iconName={`theme${capitalizeStr(theme)}`} iconSize={32} onClick={toggleTheme} />
+      <h1 className="board--title font-xxl">Retro Board</h1>
+      <div className="navbar--btns flex place-center">
+        <div className="navbar--control-btns flex place-center">
+          <IconButton
+            imgClassList={'timer-icon'}
+            iconName={"timer"}
+            iconSize={32} />
+          <div className="separator"></div>
+          <IconButton
+            imgClassList={'members-icon'}
+            iconName={"teamMembers"}
+            iconSize={32} />
+        </div>
+        <div className="separator"></div>
+        <div className="navbar--theme-btn">
+          <IconButton
+            imgClassList={'theme-icon'}
+            iconName={`theme${capitalizeStr(theme)}`}
+            iconSize={32}
+            onClick={toggleTheme} />
+        </div>
       </div>
     </nav>
   )
