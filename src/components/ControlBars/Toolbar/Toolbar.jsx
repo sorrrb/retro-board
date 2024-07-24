da16de59aada612ae16b45ec25c40383b67a5d6a
Christopher Druke <cdrukepike@gmail.com>
2024-07-23 23:53:07 -0400

    Implement component structure & basic logic

---
+++ src/components/ControlBars/Toolbar/Toolbar.jsx
@@ -0,0 +1,77 @@
+import { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { ThemeContext } from '../../../Contexts';
+import '../ControlBars.css';
+import './Toolbar.css';
+import IconButton from "../../Buttons/IconButton/IconButton";
+
+function Toolbar() {
+  const theme = useContext(ThemeContext);
+  const [visibility, setVisibility] = useState(true);
+  const [layoutDirection, setLayoutDirection] = useState('vertical');
+  const [itemSearch, setItemSearch] = useState('');
+
+  return (
+    <div className={`toolbar theme--${theme} flex align-center`}>
+      <div className="toolbar--controls flex align-center">
+        <input
+          className="toolbar--context flex align-center font-med padding-sm"
+          name="toolbarContext"
+          id="toolbarContext"
+          placeholder="Set the context or state of affairs for your sprint..."
+          aria-placeholder="Set the context or state of affairs for your sprint..."
+        />
+        <IconButton
+          imgClassList={"visibility-icon"}
+          iconFill={"FFFFFF"}
+          iconName={`visibility${visibility ? "Shown" : "Hidden"}`}
+          iconTitle={visibility ? "Hide Items" : "Show Items"}
+          iconSize={16}
+          iconStyle={"ios-glyphs"}
+          onClick={() => { visibility ? setVisibility(false) : setVisibility(true) }}
+        />
+        <input
+          className="toolbar--search flex align-center font-med padding-xs"
+          type="search"
+          name="toolbarSearch"
+          id="toolbarSearch"
+          placeholder="Search"
+          aria-placeholder="Search"
+          value={itemSearch}
+          onChange={e => setItemSearch(e.target.value)}
+        />
+        <div className="toolbar--layout-container flex place-center padding-xxs">
+          <IconButton
+            btnClassList={`flex place-center padding-xxxs ${layoutDirection === "vertical" ? "active-layout" : ""}`}
+            imgClassList={"vertical-layout-icon"}
+            iconFill={"FFFFFF"}
+            iconName={`layoutVertical`}
+            iconTitle={"Set layout: Vertical"}
+            iconStyle={"ios-filled"}
+            iconSize={16}
+            onClick={() => setLayoutDirection("vertical")}
+          />
+          <IconButton
+            btnClassList={`flex place-center padding-xxxs ${layoutDirection === "horizontal" ? "active-layout" : ""}`}
+            imgClassList={"horizontal-layout-icon"}
+            iconFill={"FFFFFF"}
+            iconName={`layoutHorizontal`}
+            iconTitle={"Set layout: Horizontal"}
+            iconStyle={"ios-filled"}
+            iconSize={16}
+            onClick={() => setLayoutDirection("horizontal")}
+          />
+        </div>
+        <div className="separator"></div>
+      </div>
+      <div className="toolbar--nav-menu grid">
+      </div>
+    </div>
+  )
+}
+
+Toolbar.propTypes = {
+
+}
+
+export default Toolbar;
